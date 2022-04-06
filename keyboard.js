9a49cb81cae495c58186d6d27179be3566d5b950
abhay0480-eng <abhay0480sharma@gmail.com>
2022-04-06 22:53:55 +0530

    keyboard designing task 1

---
+++ keyboard.js
@@ -0,0 +1,6 @@
+let displayletter = document.getElementById("display")
+let letter = document.getElementsByClassName("letter")
+let get1 = letter.textContent
+function display() {
+    displayletter.textContent = "abhay"+ get1
+}
